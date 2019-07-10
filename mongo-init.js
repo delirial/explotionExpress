438fd7c39cc63e8529f4393487248c39db78c0ce
Delirial <elarcaeldelirio@gmail.com>
2019-07-10 15:09:49 +0100

    Added data

--- mongo-init.js
+++ mongo-init.js
@@ -13,6 +13,9 @@ db.createUser(
 
 use movies;
 db.movies.insertMany([
-    {},
-    {}
+    {title:"Toy Story 4",year:2019,genre:["Comedy","Animation"],duration:100},
+
+    {title:"Vengadores:Endgame",year:2019,genre:["SuperHero","Action"],duration:181},
+    
+    {title:"Glass",year:2019,genre:["Terror","Suspense"],duration:129}
 ])
