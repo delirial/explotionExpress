9d960f70395d642abf3fc33dc3b91b1bd3ad4369
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-07-10 15:33:00 +0100

    Hacking

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker-compose --version
-docker-compose up --build -d mongodb
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o docker-compose
+chmod +x docker-compose
+./docker-compose --version
+./docker-compose up --build -d mongodb
