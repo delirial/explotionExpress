d6e55534c88b0d01364a28bc671e7c08774ca823
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-07-10 15:38:07 +0100

    Docker?

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
+apt-get update
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+apt-get update
+apt-get install -y docker-engine
 curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o docker-compose
 chmod +x docker-compose
 ./docker-compose --version
