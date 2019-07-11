2ceb56efdea141dfda312ab230530f9109fc78ea
ArcaDelDelirio <elarcadeldelirio@gmail.com>
2019-07-11 15:32:50 +0100

    Add PORT env variable

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,12 @@ app.use('/users', usersRouter);
 app.use('/movies', moviesRouter);
 app.use('/games', gamesRouter);
 
-app.listen(3000, () => console.log('Example app listening on port 3000'));
+const port = process.env.PORT;
+
+/*
+he web process must listen for HTTP traffic on $PORT,
+which is set by Heroku. EXPOSE in Dockerfile is not respected,
+but can be used for local testing. Only HTTP requests are
+supported.*/
+
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
