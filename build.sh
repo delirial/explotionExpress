b0779a4d6bab25256c43f6b8429d8ab4505a527a
Delirial <elarcaeldelirio@gmail.com>
2019-07-14 16:13:03 +0100

    Add new travis matrix builder

--- build.sh
+++
@@ -1,9 +0,0 @@
-apt-get update
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
-apt-get update
-apt-get install -y docker-engine
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o docker-compose
-chmod +x docker-compose
-./docker-compose --version
-./docker-compose up --build -d mongodb
